f22b2f1838d150427e09a75929982ad88420690d
Deladriere Enguerrand <deladrie@insa-20721>
2022-04-08 12:15:07 +0200

    dijkstra

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -1,5 +1,14 @@
 package org.insa.graphs.algorithm.shortestpath;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.insa.graphs.algorithm.utils.BinaryHeap;
+import org.insa.graphs.model.Arc;
+import org.insa.graphs.model.Graph;
+import org.insa.graphs.model.Node;
+ 
+
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
     public DijkstraAlgorithm(ShortestPathData data) {
@@ -10,7 +19,60 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
     protected ShortestPathSolution doRun() {
         final ShortestPathData data = getInputData();
         ShortestPathSolution solution = null;
-        // TODO:
+        
+        Graph graph = data.getGraph(); //Initialisation
+        Label[] listLabel =new Label[graph.size()];//Creation du tableau de Label
+        List<Node> listNodes= graph.getNodes();//Recuperer la liste de nodes du graph
+        BinaryHeap<Label> Tas = new BinaryHeap<Label>();
+        
+        for (Node noeud : listNodes) {
+        	listLabel[noeud.getId()] = new Label(noeud,false,1.0/0.0);
+        }
+        
+        listLabel[data.getOrigin().getId()].setCost(0.0);
+        Tas.insert(listLabel[data.getOrigin().getId()]);
+        
+        boolean cheminTrouve=false;
+        
+        while(!Tas.isEmpty() || !cheminTrouve) {
+        	
+        	Label x = Tas.findMin();
+        	Tas.remove(x);
+        	x.setMark(true);
+        	notifyNodeMarked(x.getcurrentNode());
+        	for( Arc a : graph.get(x.getcurrentNode().getId()).getSuccessors()) {
+        		Label y = listLabel[a.getDestination().getId()];
+        		
+        		if(!y.getMark()) {
+        			y.setCost(Math.min(y.getCost(),x.getCost()+a.getLength()));
+        			if(y.getCost()==x.getCost()+a.getLength()){
+        				if(y.getCost()!=1.0/0.0) {
+        					Tas.insert(y);
+        				}
+        				Tas.insert(y);
+        				notifyNodeReached(a.getDestination());
+        				y.setFather(a);
+        				
+        			}
+ 
+        		}
+        		
+        	}
+        	if(x.getcurrentNode()==data.getDestination()) {
+    			cheminTrouve=true;
+    		}
+        	
+        	
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
         return solution;
     }
 
--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
@@ -3,15 +3,51 @@ package org.insa.graphs.algorithm.shortestpath;
 import org.insa.graphs.model.Arc;
 import org.insa.graphs.model.Node;
 
-public class Label {
+public class Label implements Comparable<Label>{
 	
-	Node currentNode ;
-	boolean mark;
-	double cost;
-	Arc father;
+	private Node currentNode ;
+	private boolean mark;
+	private double cost;
+	private Arc father;
+	
+	public Label(Node currentNode, boolean mark , double cost) {
+		this.currentNode = currentNode;
+		this.mark=mark;
+		this.cost=cost;
+	}
 	
 	public double getCost() {
 		return cost;
+	}
+	
+	public Node getcurrentNode() {
+		return this.currentNode;
+	}
+	
+	public boolean getMark() {
+		return this.mark;
+	}
+	
+	public void setFather(Arc x) {
+		this.father=x;
+	}
+	
+	public void setCost(double cost) {
+		this.cost=cost;
+	}
+	
+	public void setMark(boolean mark) {
+		this.mark=mark;
+	}
+	
+	
+	public int compareTo(Label a) {
+		int res;
+		if(this.getCost()<a.getCost())res=-1;
+		if(this.getCost()>a.getCost())res=1;
+		else res=0;
+		
+		return res;
 	}
 
 }
